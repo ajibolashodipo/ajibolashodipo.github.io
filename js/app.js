e291cda0d9e1608c1e5343ab9b5d1dfd1c345bfb
ajibolashodipo <shodipoajibola@gmail.com>
2020-05-29 14:48:20 +0100

    couldn't figure out nav transition. we live to fight another day

--- js/app.js
+++ js/app.js
@@ -4,8 +4,7 @@ const myNav = document.querySelector(".my-nav")
 
 hamburger.addEventListener("click", () => {
   navigation.classList.toggle("my-nav")
-  myNav.style.transition = "background-image 3s ease-in"
-  console.log("ajibola")
+  myNav.style.transition = "all 3s ease-in"
 })
 
 // alert("sdfgsdfgsd")
