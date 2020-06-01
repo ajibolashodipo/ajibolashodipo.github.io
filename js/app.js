281b14252f705ac195c2826e0ccb418c2a1647cc
ajibolashodipo <shodipoajibola@gmail.com>
2020-06-02 00:46:15 +0100

    added grid hover animation and loading page animation

--- js/app.js
+++ js/app.js
@@ -3,6 +3,11 @@ const navigation = document.querySelector(".my-nav-stopgap")
 const myNav = document.querySelector(".my-nav")
 const square = document.getElementById("square1")
 
+// loading animation
+$(window).on("load", function () {
+  $(".loader-wrapper").delay(1200).fadeOut(500)
+})
+
 hamburger.addEventListener("click", () => {
   console.log("ajibola")
   navigation.classList.toggle("my-nav")
