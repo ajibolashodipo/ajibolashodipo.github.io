acef6de14f575e333c4e1d1ceb78691a2a9586d1
ajibolashodipo <shodipoajibola@gmail.com>
2020-06-03 01:29:21 +0100

    added little styling

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 const hamburger = document.querySelector(".navbar-ish")
 const navigation = document.querySelector(".my-nav-stopgap")
 const myNav = document.querySelector(".my-nav")
-const square = document.getElementById("square1")
+// const square = document.getElementById("square1")
 
 // loading animation
 $(window).on("load", function () {
@@ -9,15 +9,5 @@ $(window).on("load", function () {
 })
 
 hamburger.addEventListener("click", () => {
-  console.log("ajibola")
   navigation.classList.toggle("my-nav")
-  // square.style.left = "25%"
-  // square1.style.transform = "translateX(25vw)"
-  // if (navigation.classList.contains("my-nav")) {
-  //   $("#square1").animate({ right: "7.5vw" })
-  // }
-
-  //animate hamburger. turn it to X on click.
-  //animate exit of navbar
-  //animate entry of navlinks
 })
